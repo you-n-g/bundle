596d4892e25c5eecf6083259288fe3d918c878c3
Young <afe.young@gmail.com>
2016-05-12 19:17:55 +0800

    update plugins

---
+++ update_all_plugins.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+cd $DIR
+
+for d in `find . -maxdepth 1 -type d  | grep -v '.git\$' | grep -v '^.$'` ; do
+    cd $DIR/$d
+    git pull -u
+    git checkout master
+done
+
+
+
+# update YouCompleteMe
+cd  ~/.vim/bundle/YouCompleteMe/
+git submodule update --init --recursive
+bash ./install.sh  --clang-completer
+cp  ~/.vim/bundle/YouCompleteMe/third_party/ycmd/examples/.ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
+
+
+# update command-T
+cd ~/.vim/bundle/command-t/ruby/command-t/
+ruby extconf.rb && make
+
+
